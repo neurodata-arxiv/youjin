6a15951f9041241e2eada94ddbc2d2c27656b30b
youjin1207 <ylee160@jhu.edu>
2016-04-20 14:11:16 -0400

    update 160420

--- meeting/meeting0421_code.R
+++ meeting/meeting0421_code.R
@@ -4,6 +4,7 @@ library(igraph)
 elegans <- read.graph("data/c.elegans.herm_pharynx_1.graphml", format = "graphml")
 soma_pos <- V(elegans)$soma_pos 
 
+## plot neuronal network & soma position
 png("figure/somapos.png")
 igraph.options(vertex.size=4.5, vertex.label=NA,
                edge.arrow.size=0.5)
@@ -20,3 +21,39 @@ legend("topright", c("<=0.1", "<=0.2", "<=0.5", "<=0.7", ">0.7"),
        col = c("lightblue1", "skyblue", "skyblue3", "dodgerblue", "dodgerblue4"),
        pch = 20)
 dev.off()
+
+## plot neuronal network & soma direction
+tmp <- V(elegans)$cell_name
+tmp <- strsplit(tmp, "")
+
+cell <- c()
+for(i in 1:length(tmp)){
+  cell[i] <- ifelse(tmp[[i]][length(tmp[[i]])] == "L", "L", NA)
+  cell[i] <- ifelse(tmp[[i]][length(tmp[[i]])] == "R", "R", cell[i])
+  cell[i] <- ifelse(is.na(cell[i]), cell_class[i], cell[i])
+}
+png("figure/cell.png")
+igraph.options(vertex.size=4.5, vertex.label=NA,
+               edge.arrow.size=0.5)
+V(elegans)$color <- "grey"
+V(elegans)[cell == "L"]$color <- "hotpink"
+V(elegans)[cell == "R"]$color <- "dodgerblue"
+E(elegans)$width <- 0.2
+E(elegans)$color <- "ivory3"
+plot(elegans,layout=layout.fruchterman.reingold,
+     main = "Neuronal Network")
+legend("topright", c("Left", "Right", "others"),
+       col = c("hotpink", "dodgerblue", "grey"),
+       pch = 20)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
